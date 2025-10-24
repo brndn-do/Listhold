d61a450729abbd517fb688ad5a4bf8b5d0b59fb1
brndn-do <dobrandon05@gmail.com>
2025-10-24 12:45:55 -0500

    refactor: refactor Roster.tsx

--- src/components/Roster.tsx
+++ src/components/Roster.tsx
@@ -16,33 +16,32 @@ interface RosterProps {
 
 const Roster = ({
   eventData,
-  eventLoading,
-  eventError,
   signups,
   signupsLoading,
   signupsError,
 }: RosterProps) => {
-  const { user, loading } = useAuth();
+  const { user } = useAuth();
 
+  // has the user already joined the list? (must be signed in first, if this is true, user is true)
   const alreadyJoined = useMemo(() => {
-    if (!user) return false;
-    return signups?.some((s) => s.uid === user.uid);
+    return user && signups && signups?.some((s) => s.uid === user?.uid);
   }, [user, signups]);
 
+  // are there spots open?
   const spotsOpen = useMemo(() => {
-    if (!eventData || eventLoading || eventError || !signups) return false;
-    return (eventData.capacity ?? 0) > signups.length;
-  }, [eventData, eventLoading, eventError, signups]);
+    return (
+      signups && signups.length && (eventData?.capacity ?? 0) > signups.length
+    );
+  }, [eventData, signups]);
 
   return (
     <div className='w-full h-full flex flex-col items-center gap-2'>
-      {signupsLoading && <p>Loading...</p>}
-      {signupsError && <p>Error: {signupsError.message}</p>}
-
       <h2 className='text-2xl font-bold'>Roster:</h2>
 
       <div className='flex flex-col items-center border-1 w-[100%] md:w-[50%] lg:w-[40%] xl:w-[30%] 2xl:w-[25%] p-4 min-h-[50vh] rounded-2xl'>
-        {/* Display list of signups if available */}
+        {signupsLoading && <p>Loading...</p>}
+        {signupsError && <p>Error: {signupsError.message}</p>}
+
         {signups && (
           <ul className='flex flex-col items-center w-full'>
             {signups.map((signup, i) => (
@@ -51,34 +50,36 @@ const Roster = ({
           </ul>
         )}
 
-        {/*Display button for joining only if user is signed in, has not joined yet, and there are spots open*/}
-        {!loading && user && !signupsLoading && !signupsError && !alreadyJoined && spotsOpen && (
-          <button className='self-end mt-auto focus:outline-none text-sm text-white bg-purple-700 hover:bg-purple-800 font-medium rounded-lg text-sm px-3.5 py-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900 hover:cursor-pointer'>
-            Join the list
-          </button>
-        )}
-        {!user && (
+        {signups && !user && (
           <button
             aria-disabled='true'
             className='opacity-30 self-end mt-auto focus:outline-none text-sm text-white bg-purple-700 font-medium rounded-lg text-sm px-3.5 py-2 dark:bg-purple-700 dark:focus:ring-purple-900 hover:cursor-not-allowed'
           >
-            Sign in to join the roster.
+            Sign in to join the list.
           </button>
         )}
-        {alreadyJoined && (
+
+        {signups && alreadyJoined && (
           <button
             aria-disabled='true'
             className='opacity-30 self-end mt-auto focus:outline-none text-sm text-white bg-purple-700 font-medium rounded-lg text-sm px-3.5 py-2 dark:bg-purple-700 dark:focus:ring-purple-900 hover:cursor-not-allowed'
           >
-            You have already joined the roster.
+            You have already joined the list.
           </button>
         )}
-        {!spotsOpen && (
+
+        {signups && user && !alreadyJoined && !spotsOpen && (
           <button
             aria-disabled='true'
             className='opacity-30 self-end mt-auto focus:outline-none text-sm text-white bg-purple-700 font-medium rounded-lg text-sm px-3.5 py-2 dark:bg-purple-700 dark:focus:ring-purple-900 hover:cursor-not-allowed'
           >
             There are no spots left.
+          </button>
+        )}
+
+        {signups && user && !alreadyJoined && spotsOpen && (
+          <button className='self-end mt-auto focus:outline-none text-sm text-white bg-purple-700 hover:bg-purple-800 font-medium rounded-lg text-sm px-3.5 py-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900 hover:cursor-pointer'>
+            Join the list
           </button>
         )}
       </div>
