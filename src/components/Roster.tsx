1597814f39a475797157e198657cf6b243794813
brndn-do <dobrandon05@gmail.com>
2025-10-28 11:46:16 -0500

    feat(signups): Add loading for leave button and change style

--- src/components/Roster.tsx
+++ src/components/Roster.tsx
@@ -125,9 +125,11 @@ const Roster = ({ eventId, eventData, signups, signupsLoading, signupsError }: R
           <button
             onClick={handleLeave}
             disabled={isLoading}
+            
             className='inline-flex self-end mt-auto focus:outline-none text-sm text-white bg-purple-700 hover:bg-purple-800 font-medium rounded-lg text-sm px-3.5 py-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900 hover:cursor-pointer'
           >
-            Leave This Event
+            {isLoading && <Spinner />}
+            {isLoading ? 'Leaving...' : 'Leave This Event'}
           </button>
         )}
 
