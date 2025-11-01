2298e321717cb4255d56da16ceb530150b42ed29
brndn-do <dobrandon05@gmail.com>
2025-11-01 15:47:24 -0500

    feat: Add viewWaitlist state, change scrollbar

--- src/components/EventList.tsx
+++ src/components/EventList.tsx
@@ -12,24 +12,15 @@ const EventList = ({ signups }: EventListProps) => {
       <ol className='flex-1 flex flex-col w-full overflow-y-auto scrollbar scrollbar-thin items-center list-decimal list-inside'>
         {signups.map((signup) => (
           <li
-            className={user?.uid === signup.uid ? 'text-purple-700 dark:text-purple-600' : ''}
+            className={user?.uid === signup.uid ? 'text-purple-700 dark:text-purple-500' : ''}
             key={signup.uid}
           >{`${signup.displayName}`}</li>
         ))}
-        {[
-          1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25,
-          26, 27, 28, 29, 30,
-        ].map((num) => (
-          <li key={num}>{`Salvador Lance Lopez`}</li>
-        ))}
-        {Array.from({ length: 15 }, (_, i) => `Hidden ${i + 1}`).map((str) => (
-          <li aria-hidden='true' className='opacity-0' key={str}></li>
-        ))}
       </ol>
       {/* gradient fade hint */}
-      <div className='pointer-events-none absolute top-0 p-2 w-full h-[5%] bg-gradient-to-b from-[#f6f6f6ff] dark:from-[#191919] to-transparent rounded-t-2xl' />
+      <div className='pointer-events-none absolute top-0 p-2 w-full h-4 bg-gradient-to-b from-[#f6f6f6ff] dark:from-[#191919] to-transparent rounded-t-2xl' />
       {/* gradient fade hint */}
-      <div className='pointer-events-none absolute bottom-0 p-2 w-full h-[30%] bg-gradient-to-t from-[#f6f6f6ff] dark:from-[#191919] to-transparent rounded-b-2xl' />
+      <div className='pointer-events-none absolute bottom-0 p-2 w-full h-4 bg-gradient-to-t from-[#f6f6f6ff] dark:from-[#191919] to-transparent rounded-b-2xl' />
     </div>
   );
 };
--- src/components/EventListWrapper.tsx
+++ src/components/EventListWrapper.tsx
@@ -44,6 +44,7 @@ const EventListWrapper = ({
   const [cooldown, setCooldown] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
   const [functionError, setFunctionError] = useState<string | null>(null);
+  const [ viewWaitlist, setViewWaitlist ] = useState(false); // is the user viewing waitlist or the main list?
   const COOLDOWN_TIME = 2500; // how long to disable button after successful join/leave
   const ERROR_TIME = 7500; // how long to display error before allowing retries
 
@@ -99,10 +100,11 @@ const EventListWrapper = ({
   };
 
   return (
+
     <div className='w-full h-full flex flex-col items-center gap-1'>
-      <div className='flex gap-24 text-lg font-[400]'>
-        <button className='text-purple-700 dark:text-purple-500 underline hover:cursor-pointer'>Signups</button>
-        <button className='underline hover:cursor-pointer'>Waitlist</button>
+      <div className='flex gap-24 text-lg pb-1'>
+        <button onClick={() => setViewWaitlist(false)} className={`${!viewWaitlist ? 'text-purple-700 dark:text-purple-500 ' : ''}underline hover:cursor-pointer`}>Signups</button>
+        <button onClick={() => setViewWaitlist(true)} className={`${viewWaitlist ? 'text-purple-700 dark:text-purple-500 ' : ''}underline hover:cursor-pointer`}>Waitlist</button>
       </div>
 
       <div className='relative flex flex-col items-center border h-[52dvh] w-full py-2 px-1 rounded-2xl'>
