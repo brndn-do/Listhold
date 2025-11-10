71b2d1f1ee21546fca691b39f664aa0923f537bb
brndn-do <dobrandon05@gmail.com>
2025-11-09 21:42:31 -0600

    feat: Update types

--- src/types.ts
+++ src/types.ts
@@ -1,23 +1,43 @@
 import { Timestamp } from 'firebase/firestore';
 
+// stores the correct answer(s) for a prompt
+type valid =
+| boolean /* yes/no question can only have one correct boolean answer */
+| string /* select question can only have one correct string answer */
+| string[] /* a checkbox question can have a single combination of string options that is correct */
+| string[][] /* a checkbox question can have multiple combinations of string options that are correct */;
+
+// eslint-disable-next-line
+interface FuturePromptData {
+  id: string;
+  order: number; // The order the prompt shows up e.g., 1, 2, 3... to define the sequence
+  type: 'notice' | 'yes/no' | 'text' | 'select' | 'checkbox' | 'notice';
+  text: string;
+  visibility?: 'public' | 'private'; // Whether a user's answer to a non-notice prompt can be shown publicly or only to admins/authorized users
+  options?: string[]; // list of options for select/checkbox questions
+  
+  // requiring a specific answer to this prompt for signup
+  validation?: {
+    valid: valid;
+    errorMessage?: string; // custon error message for the user if they fail validation
+  };
+  
+  // choose whether or not this prompt appears based on a previous answer
+  displayCondition?: {
+    promptId: string; // The ID of the prompt this one depends on
+    valid: valid;
+  };
+}
+
 /**
  * Represents a Firestore prompt document, with document ID.
  */
 export interface PromptData {
   id: string;
-  order: number; // The order the prompt shows up e.g., 1, 2, 3... to define the sequence
-  type: 'yes/no' | 'text' | 'select' | 'checkbox' | 'notice';
+  order: number;
+  type: 'notice' | 'yes/no';
   text: string;
-  options?: string[]; // list of options for select/checkbox questions
-  validation?: {
-    // optional: requiring a specific answer to this prompt for signup
-    valid: // the correct answer to expect
-    | boolean /* yes/no question can only have one correct boolean answer */
-      | string /* select question can only have one correct string answer */
-      | string[] /* a checkbox question can have a single combination of string options that is correct */
-      | string[][] /* a checkbox question can have multiple combinations of string options that are correct */;
-    errorMessage?: string; // custon error message for the user if they fail validation
-  };
+  visibility?: 'public' | 'private'
 }
 
 /**
