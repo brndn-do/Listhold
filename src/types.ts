8fae0b344767497c89322489ccb680762515dca8
brndn-do <dobrandon05@gmail.com>
2025-11-09 21:16:18 -0600

    feat: Add prompt type (questions + notices)

--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,26 @@
 import { Timestamp } from 'firebase/firestore';
 
 /**
+ * Represents a Firestore prompt document, with document ID.
+ */
+export interface PromptData {
+  id: string;
+  order: number; // The order the prompt shows up e.g., 1, 2, 3... to define the sequence
+  type: 'yes/no' | 'text' | 'select' | 'checkbox' | 'notice';
+  text: string;
+  options?: string[]; // list of options for select/checkbox questions
+  validation?: {
+    // optional: requiring a specific answer to this prompt for signup
+    valid: // the correct answer to expect
+    | boolean /* yes/no question can only have one correct boolean answer */
+      | string /* select question can only have one correct string answer */
+      | string[] /* a checkbox question can have a single combination of string options that is correct */
+      | string[][] /* a checkbox question can have multiple combinations of string options that are correct */;
+    errorMessage?: string; // custon error message for the user if they fail validation
+  };
+}
+
+/**
  * Represents a Firestore signup document, with user's UID.
  */
 export interface SignupData {
@@ -29,8 +49,6 @@ export interface EventData {
   // Future feat
   // eslint-disable-next-line
   rules?: Record<string, any>;
-
-  // subcollection: questions
 }
 
 /**
