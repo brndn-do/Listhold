a8c8d27e408dce271836982c5bd9e6363801fc03
brndn-do <dobrandon05@gmail.com>
2025-11-15 13:09:37 -0600

    ui: Change size of button

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -12,7 +12,7 @@ const Button = ({ onClick, content, disabled }: ButtonProps) => {
       type='button'
       onClick={onClick}
       disabled={!!disabled}
-      className={`${disabled ? 'opacity-35' : 'hover:cursor-pointer hover:bg-purple-800 dark:hover:bg-purple-700'} inline-flex text-sm text-white bg-purple-700 font-medium rounded-lg text-sm px-3.5 py-2 dark:bg-purple-600 dark:focus:ring-purple-900`}
+      className={`${disabled ? 'opacity-35' : 'hover:cursor-pointer hover:bg-purple-800 dark:hover:bg-purple-700'} inline-flex text-sm text-white bg-purple-700 font-medium rounded-lg text-sm px-3.5 py-1.75 dark:bg-purple-600 dark:focus:ring-purple-900`}
     >
       {content}
     </button>
