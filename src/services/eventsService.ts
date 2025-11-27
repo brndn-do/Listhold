c7b0b571620bcf02aae489472ee93bbab4d93927
brndn-do <dobrandon05@gmail.com>
2025-11-27 01:39:33 -0600

    fix: Update import path

--- src/services/eventsService.ts
+++ src/services/eventsService.ts
@@ -1,5 +1,6 @@
 import { db } from '@/lib/firebase';
-import { EventData, WithId } from '@/types';
+import { EventData } from '@/types/eventData';
+import { WithId } from '@/types/withId';
 import { collection, getDocs, orderBy, query, Timestamp, where } from 'firebase/firestore';
 
 export const getEventsByOrgId = async (orgId: string): Promise<WithId<EventData>[]> => {
